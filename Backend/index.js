b9fb182f7f4c74931a9ac0c7ee609ed50dd0590d
ZaidKha2321 <zaidkhan232125@gmail.com>
2024-12-05 19:50:02 +0530

    Configure the cors policy to connect backend and frontend

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,15 @@
 import express from 'express' ;
 import './config/dotenv.config.js' ;
+import cors from 'cors' ;
+
+const corsOptions = {
+    origin : [process.env.FRONTEND_URL] ,
+    allowedHeaders : '*' ,
+    methods : ["GET", "POST", "PUT", "DELETE"] ,
+    credentials : true
+} ;
 
 const server = express() ;
+server.use(cors(corsOptions)) ;
 
 export default server ;
